18b6482978285e38df83c6d7c14d2f059a53fd1c
gustavo-rcoelho <gustavocoelho112006@gmail.com>
2026-05-06 22:40:36 -0300

    Criado notificador.py

---
+++ services/notificador.py
@@ -0,0 +1,10 @@
+class Notificador:
+
+    def notificar_emprestimo(self, email, data_devolucao):
+        ...
+
+    def notificar_devolucao(self, email, multa):
+        ...
+
+    def notificar_atraso(self, email):
+        ...
