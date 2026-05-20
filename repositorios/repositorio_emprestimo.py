787474fe02c17dba2b5f1cbb62850b0b71fba401
gustavo-rcoelho <gustavocoelho112006@gmail.com>
2026-05-20 12:38:47 -0300

    Refactor RepositorioEmprestimo with new methods

--- repositorios/repositorio_emprestimo.py
+++ repositorios/repositorio_emprestimo.py
@@ -1,22 +1,47 @@
+from models.equipamento import Notebook, Projetor, Cabo
+from models.emprestimo import Emprestimo
+import datetime
+
 class RepositorioEmprestimo:
 
-    def buscar_equipamento(self, equip_id):
-        ...
+    def __init__(self):
+        self.equipamentos = [
+            Notebook(1, "Notebook Dell", "notebook", True),
+            Projetor(2, "Projetor Epson", "projetor", True),
+            Cabo(3, "Cabo HDMI", "cabo", True),
+        ]
 
-    def salvar_emprestimo(self, emprestimo):
-        ...
+        self.emprestimos = []
+
+    def buscar_equipamento(self, equip_id):
+        for e in self.equipamentos:
+            if e.id == equip_id:
+                return e
+        return None
 
     def marcar_indisponivel(self, equip_id):
-        ...
+        eq = self.buscar_equipamento(equip_id)
+        if eq:
+            eq.disponivel = False
+
+    def marcar_disponivel(self, equip_id):
+        eq = self.buscar_equipamento(equip_id)
+        if eq:
+            eq.disponivel = True
+
+    def salvar_emprestimo(self, emprestimo: Emprestimo):
+        self.emprestimos.append(emprestimo)
 
     def buscar_emprestimo(self, emprestimo_id):
-        ...
+        for e in self.emprestimos:
+            if e.id == emprestimo_id:
+                return e
+        return None
 
     def marcar_devolvido(self, emprestimo_id):
-        ...
-
-    def marcar_disponivel(self, equipamento_id):
-        ...
+        emp = self.buscar_emprestimo(emprestimo_id)
+        if emp:
+            emp.devolvido = True
 
     def listar_emprestimos(self):
-        ...
+        return self.emprestimos
